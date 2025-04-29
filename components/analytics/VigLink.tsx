8b302f2f402405e8d0c927f5bc3b372966579271
amm10090 <amm10090@163.com>
2025-04-29 17:58:21 +0800

    fix(analytics): 优化VigLink组件中的JavaScript代码格式
    此更新主要包括：
    - 将VigLink组件中的JavaScript代码格式进行了优化，减少了换行，使代码更加紧凑。
    - 这一更改旨在提升代码的可读性和维护性，确保在页面加载时能够正确执行分析脚本。
    
    没有副作用。

--- components/analytics/VigLink.tsx
+++ components/analytics/VigLink.tsx
@@ -21,18 +21,14 @@ export function VigLink() {
     }
 
     return (
-        <Script id="viglink">
-            {`
-                var vglnk = {key: '${VIGLINK_KEY}'};
-                (function(d, t) {
-                    var s = d.createElement(t);
-                    s.type = 'text/javascript';
-                    s.async = true;
-                    s.src = '//cdn.viglink.com/api/vglnk.js';
-                    var r = d.getElementsByTagName(t)[0];
-                    r.parentNode.insertBefore(s, r);
-                }(document, 'script'));
-            `}
+        <Script type="text/javascript" id="viglink">
+            {`var vglnk = {key: '${VIGLINK_KEY}'};
+(function(d, t) {var s = d.createElement(t);
+s.type = 'text/javascript';s.async = true;
+s.src = '//cdn.viglink.com/api/vglnk.js';
+var r = d.getElementsByTagName(t)[0];
+r.parentNode.insertBefore(s, r);
+}(document, 'script'));`}
         </Script>
     );
 } 
